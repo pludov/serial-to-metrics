574f90f27cfea6ba40cdb07791ca7b5e01a66d00
Ludovic P <pludow@gmail.com>
2025-01-16 22:36:25 +0100

    Fix install perms

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# This script is used to install the serial-to-metrics service on a Raspberry Pi.
+# It will build & install the binary and setup the systemd service
+
+set -euo pipefail
 
 cargo build --release
 
 sudo cp target/release/serial-to-metrics /usr/bin/
-sudo cp serial-to-metrics.service /etc/systemd/system/
-# daemon-reload is needed to make systemd aware of the new service
+sudo cp systemd/serial-to-metrics.service /etc/systemd/system/
+
 sudo systemctl daemon-reload
 sudo systemctl enable serial-to-metrics
 sudo systemctl start serial-to-metrics
